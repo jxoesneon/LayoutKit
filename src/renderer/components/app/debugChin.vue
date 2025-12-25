9848af04ec897961fe7319933a15591ed9d3bc55
jxoesneon <joseeduardox@gmail.com>
2025-12-25 01:11:55 -0600

    Add debug component for development

---
+++ src/renderer/components/app/debugChin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="debug-chin">
+    <!-- Debug component for development -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DebugChin'
+}
+</script>
+
+<style scoped>
+.debug-chin {
+  /* Debug styles */
+}
+</style>
